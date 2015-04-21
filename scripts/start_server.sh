9e67e9089c63f120230b17bc29c4fd69e60fe8f4
James Beavers <jamesjbeavers@gmail.com>
2015-04-20 20:57:13 -0400

    add logging

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,13 +2,17 @@
 
 set -e
 
+LOG_FILE=start_server.log
+
 service docker start
 cd /grounds
 /usr/local/bin/docker-compose --version
 export PATH=/usr/local/bin:$PATH
-make pull
-docker pull redis
-make run
+make pull 2>&1 | tee -a $LOG_FILE                                                                                                                                                                                  
+echo "-----------" >> $LOG_FILE
+echo "end pull, begin run" >> $LOG_FILE
+echo "-----------" >> $LOG_FILE
+make run 2>&1 | tee -a $LOG_FILE
 cd -
 
 exit 0
